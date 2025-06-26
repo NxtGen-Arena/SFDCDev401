c986ad128223cce35ddd762a2d69af64f477b9f0
Sheetal Chougule <65972334+sheetalchougule-git@users.noreply.github.com>
2025-06-26 09:58:16 +0530

    Update traineeSearchHelper.js

--- CodeBase/Aura/Event-Handling/Application-Level/traineeSearch/traineeSearchHelper.js
+++ CodeBase/Aura/Event-Handling/Application-Level/traineeSearch/traineeSearchHelper.js
@@ -2,6 +2,7 @@
     /* fetches the list of trainees from Apex*/
     loadTrainees: function (component) {
         var action = component.get('c.getTrainees');
+        action.setParams({"accountId": component.get("v.recordId")});
         
         // Add callback method
         action.setCallback(this, function(response) {
