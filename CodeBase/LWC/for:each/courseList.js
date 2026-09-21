a4509b65631d0bbe134d9f3f7048cb0a1c7f41b6
Sheetal Chougule <65972334+sheetalchougule-git@users.noreply.github.com>
2026-09-21 12:59:08 +0530

    Create courseList.js

---
+++ CodeBase/LWC/for:each/courseList.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+export default class CourseList extends LightningElement {
+
+courses=[
+{
+id:1,
+name:'Sales Cloud',
+duration:'30 Hours',
+trainer:'Sheetal'
+},
+{
+id:2,
+name:'LWC Development',
+duration:'40 Hours',
+trainer:'John'
+},
+{
+id:3,
+name:'Health Cloud',
+duration:'35 Hours',
+trainer:'Priya'
+}
+];
+
+}
