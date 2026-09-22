2f7c2ef9cc09861fd35dd43d35f439107bc8dc55
Sheetal Chougule <65972334+sheetalchougule-git@users.noreply.github.com>
2026-09-22 13:56:25 +0530

    Update displayInstitutes.js

--- CodeBase/LWC/Decorators/Wire/displayInstitutes.js
+++ CodeBase/LWC/Decorators/Wire/displayInstitutes.js
@@ -1,11 +1,35 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 
+// Apex Methods
 import getInstitutes from '@salesforce/apex/InstituteController.getInstitutes';
+import searchInstitutes from '@salesforce/apex/InstituteController.searchInstitutes';
 
-export default class InstituteList extends LightningElement{
+export default class InstituteList extends LightningElement {
 
-@wire(getInstitutes)
+    // -----------------------------
+    // Example 1: @wire without parameter
+    // -----------------------------
+    @wire(getInstitutes)
+    institutes;
 
-institutes;
+    // -----------------------------
+    // Example 2: @wire with parameter
+    // -----------------------------
+    @track selectedIndustry = 'Consulting';
 
+    @wire(searchInstitutes, { city: '$selectedIndustry' })
+    filteredInstitutes;
+
+    // Handle dropdown value change
+    handleCityChange(event) {
+        this.selectedIndustry = event.target.value;
+    }
+
+    // Picklist options
+    industryOptions = [
+        { label: 'Healthcare', value: 'Healthcare' },
+        { label: 'Pharma', value: 'Pharma' },
+        { label: 'Consulting', value: 'Consulting' },
+        { label: 'Energy', value: 'Energy' }
+    ];
 }
